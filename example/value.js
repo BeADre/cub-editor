f0f85ade250de5aa702ec9990a0c1b6f174734b8
BeADre <beadre_wang@163.com>
2022-12-22 17:08:29 +0800

    refactor: refactor style

--- example/value.js
+++ example/value.js
@@ -1,4 +1,92 @@
-export default `
-1. And they can be ordered
-\t1. and nested
-2. and contain *styling*`;
+export default '# 个人信息\n' +
+'- 王浩/男/1996\n' +
+'- 本科/宜宾学院/机械电子\n' +
+'- 手机：15775973717\n' +
+'- 邮箱: beadre_wang@163.com\n' +
+'- 求职意向：Web前端开发工程师\n' +
+'- Github: https://github.com/BeADre\n' +
+'# 工作经历\n' +
+'- 杭州安恒信息技术股份有限公司\n' +
+'     - 在职时间：2021年1月 ～ 至今\n' +
+'- 北京致远互联科技软件股份有限公司\n' +
+'     - 在职时间：2020年4⽉ ~ 2021年1⽉\n' +
+'     - 离职原因：组织架构改变导致⼈员调整\n' +
+'- 北京易迅理想科技有限公司\n' +
+'     - 在职时间：2019年4月 ~ 2020年4月\n' +
+'     - 离职原因：因疫情原因导致薪酬产⽣了较⼤变化\n' +
+'- 成都世纪顶点科技有限公司\n' +
+'     - 在职时间：2017年7月 ~ 2019年3月 （含实习时间）\n' +
+'     - 离职原因：与个⼈职业规划冲突\n' +
+'# 前端技能\n' +
+'- 基础：html5, css3（及其预处理器）, javascript（esnext, nodejs）, typescript\n' +
+'- 框架：vue, react（及其配套技术栈）\n' +
+'- 组件库：varlet-ui, ant-design, element, material-ui, vant等\n' +
+'- 图形库：echarts.js, antv, three.js等\n' +
+'- 单元测试：jest, vitest等\n' +
+'- 构建工具：vite, webpack, rollup等\n' +
+'- 代码风格：ESLint, Prettier, husky, lint-staged, editorconfig等\n' +
+'- 版本工具：git（git flow工作流及Angular commit message规范）, svn\n' +
+'# 项目经验\n' +
+'## 公司项目\n' +
+'**ps：项⽬时间从晚(现在)到早(2017年)排序，只列举部分项⽬**\n' +
+'### TGFW(明御下一代防火墙)\n' +
+'- 项目简介：下一代防火墙的管理平台，能直接在界面上对防火墙进行数据管理、命令下发等\n' +
+'- 技术栈：vue2全家桶、ant-design-vue、axios、echarts等\n' +
+'- 工作内容：\n' +
+'     1. 依据需求文档开发新模块并对已有功能进行维护\n' +
+'     2. 推动组件化开发，降低业务代码复杂度，提高开发效率\n' +
+'     3. 制定团队内部的代码编写规范、git提交规范并review代码抓代码质量\n' +
+'     4. 负责攻克项目中的重难点问题以及指导组内初级同学完成功能开发\n' +
+'### IDMC(安恒设备智能管理中心)\n' +
+'- 项目简介：针对防火墙的集中管控平台，能对被管控的任意一台防火墙进行数据管理、下发命令等\n' +
+'- 技术栈：vue3全家桶、ant-design-vue、axios、echarts等\n' +
+'- 工作内容：\n' +
+'     1. 技术调研并选型，从零搭建脚手架\n' +
+'     2. 独自完成第一个版本的功能并成功上线\n' +
+'     3. 编写基础插件（自动生成代理、自动引入公共样式等）以及功能拓展（国际化、多版本等）\n' +
+'     4. 编写各种composition-api以及cli命令（如table和form生成器）提高开发效率\n' +
+'### 聊天记录管理平台\n' +
+'- 项目简介：管理用户导入的各种社交软件聊天记录的一个平台\n' +
+'- 技术栈：react全家桶、ant-design、axios\n' +
+'- 工作内容\n' +
+'     1. 聊天记录正常显示，即常用的无限滚动方案调研及选择\n' +
+'     2. 接入谷歌地图并标注用户的活跃地点然后展示对比图\n' +
+'     3. 媒体查询多端适配\n' +
+'### ⼤屏⼈脸识别系统\n' +
+'- 项目简介：一个使用在线摄像头拍摄照片并将照片分析结果和统计结果进行显示的平台\n' +
+'- 技术栈：react全家桶、ant-design、axios、echarts、原生nodejs\n' +
+'- 工作内容\n' +
+'     1. 分别调用网络摄像头（海康威视）和本地摄像头进行照片拍摄\n' +
+'     2. 启动node服务器对照片文件（主要是网络摄像头照片）进行管理\n' +
+'     3. 照片分析结果和统计结果的显示\n' +
+'### 未来国际官网\n' +
+'- 项目简介:  公司官网页面（包括展示端、数据管理平台、服务端）\n' +
+'- 技术栈：react全家桶、ant-design、axios、egg + mongodb\n' +
+'- 工作内容\n' +
+'     - 个人负责所有三端所有开发，其中官网主要是新闻和公告的数据获取，管理端主要是对应数据的编辑，所以服务端主要的逻辑就是对应两个数据的增删查改以及管理端的账号验证\n' +
+'### 3D内⽹态势\n' +
+'- 项目简介：3D可视化公司资产数据\n' +
+'- 技术栈： jquery、three.js、tween.js\n' +
+'- 工作内容\n' +
+'     1. 主要对3D展示功能预研以及实现\n' +
+'     2. 交互事件以及攻击状态和动画的集成\n' +
+'## 开源项目\n' +
+'**ps：只列举部分项⽬**\n' +
+'###  varlet（Member）\n' +
+'- star：**3577（如果您能点个star就更好了^_^）**\n' +
+'- 项目描述：**基于 Vue3 的 Material design 风格移动端组件库**\n' +
+'- [项目地址](https://github.com/varletjs/varlet)\n' +
+'- [文档地址](https://varlet.gitee.io/varlet-ui/#/zh-CN/index)\n' +
+'- 项目成绩：\n' +
+'     -  **尤雨溪2021年Vue conf line 推荐移动端框架**\n' +
+'     - **入选2022年Gitee最有价值开源项目**\n' +
+'### element-plus（Contributor）\n' +
+'- star：**18011**\n' +
+'- 项目描述：**A Vue.js 3.0 UI Library made by Element team**\n' +
+'- [项目地址](https://github.com/element-plus/element-plus)\n' +
+'- [文档地址](https://element-plus.org/zh-CN/)\n' +
+'### YesPlayMusic（Contributor）\n' +
+'- star：**20189**\n' +
+'- 描述：**高颜值的第三方网易云播放器**\n' +
+'- [项目地址](https://github.com/qier222/YesPlayMusic)\n' +
+'- [文档地址](https://music.qier222.com/)'
